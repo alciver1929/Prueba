f10316001d0455c2fd240f1101c6e47fb618aef1
alciver <65777602+alciver@users.noreply.github.com>
2020-11-30 17:28:30 -0500

    Agregar archivos de proyecto.

---
+++ Prueba/ConexionBd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba
+{
+    class ConexionBd
+    {
+        string strConexion = " Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=prueba;Integrated Security=True";
+        SqlConnection connection = new SqlConnection();
+        public ConexionBd()
+        {
+            connection.ConnectionString = strConexion;
+        }
+        public void conectar()
+        {
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Conectado");
+
+            } catch (Exception e)
+            {
+                MessageBox.Show("error" + e.Message);
+            }
+        }
+    }
+   
+}
